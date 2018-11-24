cf7fcb0036a6b5a0cb0b3d9ffde8c9b29b3aef94
rlaehgns5399 <rlaehgns5398@gmail.com>
2018-11-24 20:29:58 +0900

    Implement getting a size of file

--- filewrapper.py
+++ filewrapper.py
@@ -7,6 +7,31 @@ args = parser.parse_args()
 
 
 file_size = os.path.getsize(args.file)
+# 100 바이트 이하면 가치없다고 판단하고 종료
+if file_size < 100:
+    print("파일이 너무 작아서 가치가 없습니다.")
+    exit()
 
+one_file_size = int(file_size / 10)
+
+file_size_list = [
+    one_file_size,
+    one_file_size,
+    one_file_size,
+    one_file_size,
+    one_file_size,
+    one_file_size,
+    one_file_size,
+    one_file_size,
+    one_file_size,
+    file_size - one_file_size * 9
+]
+
+print(file_size_list)
+
+file_size_index = 0
 with open(args.file, "rb") as f:
-    byte = f.read(1)
+    for i in range(1, 10):
+        byte = f.read(file_size_list[i])
+
+        print(byte)
