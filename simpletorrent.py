3060ec23e2d3429ec7abd06737297f0aa1dd8752
rlaehgns5399 <rlaehgns5398@gmail.com>
2018-11-29 14:39:12 +0900

    Implement: When other clients open client, send its port+1~port+10 to connect on sendFirstMsg function

--- simpletorrent.py
+++ simpletorrent.py
@@ -1,10 +1,16 @@
 import socket
 import threading
+import argparse
 
 lock = threading.Lock()
 
-class Server:
+parser = argparse.ArgumentParser()
+parser.add_argument("-port", help="your port")
+arg = parser.parse_args()
+
+class Server(threading.Thread):
     def __init__(self, my_port):
+        threading.Thread.__init__(self)
         self.port = my_port
         self.sock = None
         self.peers = {}
@@ -15,15 +21,27 @@ class Server:
         self.sock.bind(("localhost", self.port))
         self.sock.listen(5)
         print("Socket is opened\r\nport : " + str(self.port))
+        self.sendFirstMsg()
         while True:
             conn, addr = self.sock.accept()
-            self.peers[self.peers_count] = (conn, addr)
+            self.peers[self.peers_counter] = (conn, addr)
             self.peers_counter += 1
-            print(self.peers[self.peers.count-1] + "is entered")
+            print("{} entered".format(addr))
 
         self.sock.close()
         print("server is terminated")
 
+    def run(self):
+        self.bind()
+
+    def sendFirstMsg(self):
+        for i in range(self.port+1, self.port+10):
+            try:
+                temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                temp_socket.connect(("localhost", i))
+            except ConnectionRefusedError:
+                pass
+
 if __name__ == '__main__':
-    server = Server(30000)
-    server.bind()
+    server = Server(int(arg.port))
+    server.start()
